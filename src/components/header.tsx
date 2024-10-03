81dedf0d0c64ea6c8ac226298aae1b2adf48061c
Thyago Barbosa Rodrigues <thyago.rodrigues@hive.app>
2024-10-03 12:00:33 +0200

    Basic navbar

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,13 @@
 import { ModeToggle } from "./ui/darkmode";
+import Link from "next/link";
 
 export const Header = () => {
   return (
-    <div className="flex justify-end p-4">
+    <div className="w-full max-w-[450px] mx-auto flex flex-row items-center justify-between mb-4">
+      <div className="w-3/4 flex flex-row gap-5">
+        <Link href="/events" className="border-b-2 border-primary-foreground">Events</Link>
+        <Link href="/" className="border-b-2 border-primary-foreground">Places</Link>
+      </div>
       <ModeToggle />
     </div>
   );
