5fed11abe0d834eafebce21650cf70ca2e996ed6
Thyago Barbosa Rodrigues <thyago.rodrigues@hive.app>
2024-11-04 10:06:18 +0100

    Only logged users can post photos

--- src/components/Photos/index.tsx
+++ src/components/Photos/index.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { useStorePhoto } from '@/hooks/photos/useStorePhoto';
 import { Photo } from './Photo';
+import { useUser } from '@/hooks/users/useUser';
 
 export function Photos({ photoable, photoableType }) {
   const [photo, setPhoto] = useState(null);
@@ -9,6 +10,8 @@ export function Photos({ photoable, photoableType }) {
   const photoableId = photoable.id;
 
   const { trigger, isLoading } = useStorePhoto({ photoableId, photoableType });
+
+  const userData = useUser();
 
   const handleFileChange = (e) => {
     setPhoto(e.target.files[0]);
@@ -36,27 +39,29 @@ export function Photos({ photoable, photoableType }) {
 
   return (
     <div>
-      <div className="flex flex-col gap-2 mt-5 px-3">
-        <input
-          className="border-2 border-white-800 rounded-md p-2"
-          type="file" onChange={handleFileChange} />
-        <div className="flex flex-row align-middle gap-2">
+      {userData?.status === "authenticated" && (
+        <div className="flex flex-col gap-2 mt-5 px-3">
           <input
-            type="checkbox"
-            name="public"
-            id="public"
-            className="border-2 border-white-800 rounded-md w-4 h-auto"
-            onChange={(e) => setPublicPhoto(e.target.checked)}
-          />
-          <label htmlFor="public">Public</label>
+            className="border-2 border-white-800 rounded-md p-2"
+            type="file" onChange={handleFileChange} />
+          <div className="flex flex-row align-middle gap-2">
+            <input
+              type="checkbox"
+              name="public"
+              id="public"
+              className="border-2 border-white-800 rounded-md w-4 h-auto"
+              onChange={(e) => setPublicPhoto(e.target.checked)}
+            />
+            <label htmlFor="public">Public</label>
+          </div>
+          <button
+            className="bg-primary hover:bg-primary/90 text-white font-bold py-2 px-4 rounded"
+            onClick={handleUpload}
+          >Upload Photo
+          </button>
+          {message && <p>{message}</p>}
         </div>
-        <button
-          className="bg-primary hover:bg-primary/90 text-white font-bold py-2 px-4 rounded"
-          onClick={handleUpload}
-        >Upload Photo
-        </button>
-        {message && <p>{message}</p>}
-      </div>
+      )}
       <div className="flex flex-col w-full m-auto max-w-[450px] gap-2 mt-5 px-3">
         {photoable.photos.map((photo) => (
           <Photo
